3c4fcfaf0558570489784fadcd379b35e2da32ed
José Fonte <jose.fonte@cloudbeds.com>
2022-06-21 10:56:40 +0100

    finish class open weather map

--- open_weather_map.py
+++ open_weather_map.py
@@ -1,5 +1,6 @@
 import requests
 from functools import cache
+from datetime import datetime
 
 
 class OpenWeatherMap:
@@ -11,24 +12,40 @@ class OpenWeatherMap:
 
     @property
     @cache
-    def geocoding(self):
+    def geocoding(self) -> tuple:
         url = f"{self.URL}/geo/1.0/direct?q={self.city}&appid={self.API_KEY}"
         response = requests.get(url).json()
-        return response[0]["name"], response[0]["lat"], response[0]["lon"]
+        name, lat, lon = response[0]["name"], response[0]["lat"], response[0]["lon"]
+        return name, lat, lon
 
-    def get_weather(self):
+    def get_weather(self) -> dict:
         _, lat, lon = self.geocoding
         url = f"{self.URL}/data/2.5/weather?lat={lat}&lon={lon}&appid={self.API_KEY}"
         response = requests.get(url).json()
-        return response
+
+        description = response["weather"][0]["description"]
+        temperate_celsius = self.__kelvin_to_celsius(response["main"]["temp"])
+        temperate_farenheit = self.__kelvin_to_farenheit(response["main"]["temp"])
+        feels_like_celsius = self.__kelvin_to_celsius(response["main"]["feels_like"])
+        feels_like_farenheit = self.__kelvin_to_farenheit(response["main"]["feels_like"])
+        sunrise_time = datetime.utcfromtimestamp(response["sys"]["sunrise"] + response["timezone"]).isoformat()
+        sunset_time = datetime.utcfromtimestamp(response["sys"]["sunset"] + response["timezone"]).isoformat()
+        humidity = response["main"]["humidity"]
+        wind_seed = response["wind"]["speed"]
+        visibility = response["visibility"]
+
+        temperature = dict(celsius=round(temperate_celsius, 2), farenheit=round(temperate_farenheit, 2))
+        feels_like = dict(celsius=round(feels_like_celsius, 2), farenheit=round(feels_like_farenheit, 2))
+
+        return dict(description=description, temperature=temperature, feels_like=feels_like, humidity=humidity, sunrise_time=sunrise_time, sunset_time=sunset_time, wind_speed=wind_seed, visibility=visibility)
 
     def get_solar_radiation(self):
         url = f"https://api.openweathermap.org/data/2.5/weather?q={self.city}&appid={self.api_key}"
         response = requests.get(url).json()
         return response
 
-    def kelvin_to_celsius(kelvin: float) -> float:
+    def __kelvin_to_celsius(self, kelvin: float) -> float:
         return kelvin - 273.15
 
-    def kelvin_to_farenheit(kelvin: float) -> float:
+    def __kelvin_to_farenheit(self, kelvin: float) -> float:
         return (kelvin - 273.15) * 9/5 + 32
--- test.py
+++ test.py
@@ -1,47 +1,8 @@
-from datetime import datetime
-
-
 from open_weather_map import OpenWeatherMap
 
 
 CITY = "Lisbon"
-response = OpenWeatherMap(CITY).get_weather()
+weather = OpenWeatherMap(CITY).get_weather()
 
 
-def kelvin_to_celsius(kelvin):
-    return kelvin - 273.15
-
-def kelvin_to_farenheit(kelvin):
-    return (kelvin - 273.15) * 9/5 + 32
-
-
-temperate_celsius = kelvin_to_celsius(response["main"]["temp"])
-temperate_farenheit = kelvin_to_farenheit(response["main"]["temp"])
-
-print(temperate_celsius, temperate_farenheit)
-
-feels_like_celsius = kelvin_to_celsius(response["main"]["feels_like"])
-feels_like_farenheit = kelvin_to_farenheit(response["main"]["feels_like"])
-
-print(feels_like_celsius, feels_like_farenheit)
-
-humidity = response["main"]["humidity"]
-
-print(humidity)
-
-description = response["weather"][0]["description"]
-
-print(description)
-
-sunrise_time = datetime.utcfromtimestamp(response["sys"]["sunrise"] + response["timezone"]).isoformat()
-sunset_time = datetime.utcfromtimestamp(response["sys"]["sunset"] + response["timezone"]).isoformat()
-
-print(sunrise_time, sunset_time)
-
-wind_speed = response["wind"]["speed"]
-
-print(wind_speed)
-
-visibility = response["visibility"]
-
-print(visibility)
+print(weather)
